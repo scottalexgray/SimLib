3c2734361a8505fffd0ac0a12a39903e958dde9e
scottalexgray <scottalexgray@gmail.com>
2020-05-12 15:19:16 +0200

    copied some functions, library can now write to serial stream (this is to debug, and for messages)

--- Modem.cpp
+++ Modem.cpp
@@ -1,5 +1,6 @@
 #include "Modem.h"
 #include <Wire.h>
+#include <Arduino.h>
 //#include <SoftwareSerial.h>
 
 // TTGO T-Call pins
@@ -15,10 +16,11 @@
 //#define SerialAT Serial1
 
 
-Modem::Modem(HardwareSerial& port, char board[], char APN[], char PIN[])
+Modem::Modem(HardwareSerial& modemPort,Stream& devicePort, char board[], char APN[], char PIN[])
 {
     //SoftwareSerial Serial1(26, 27); // RX, TX
-    _serialPort = &port;
+    _modemPort = &modemPort;
+    _devicePort = &devicePort;
 
 	TwoWire I2CPower = TwoWire(0);
 
@@ -31,9 +33,64 @@ Modem::Modem(HardwareSerial& port, char board[], char APN[], char PIN[])
     digitalWrite(MODEM_POWER_ON, HIGH);
 
     // Set GSM module baud rate and UART pins
-    _serialPort->begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
-    _serialPort->write("AT\r\n");
+    _modemPort->begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+    //_serialPort->write("AT\r\n");
+    _devicePort->write("HEEEEELLLLLOOOOOO");
     
     delay(3000);
 
 }
+
+int Modem::CIPSTATUS()
+{
+    _devicePort->write("HEEEEELLLLLOOOOOO");
+    int ret = sendCmdAndWaitForResp("AT\r\n", "OK", DEFAULT_TIMEOUT);
+    return ret;
+}
+
+void Modem::sendCmd(const char* cmd)
+{
+    _modemPort->write(cmd);
+}
+
+int Modem::sendATTest(void)
+{
+    int ret = sendCmdAndWaitForResp("AT\r\n", "OK", DEFAULT_TIMEOUT);
+    return ret;
+}
+
+int Modem::waitForResp(const char* resp, unsigned int timeout)
+{
+    int len = strlen(resp);
+    int sum = 0;
+    unsigned long timerStart, timerEnd;
+    timerStart = millis();
+
+    while (1) {
+        if (_modemPort->available()) {
+            char c = _modemPort->read();
+            sum = (c == resp[sum]) ? sum + 1 : 0;
+            if (sum == len)break;
+        }
+        timerEnd = millis();
+        if (timerEnd - timerStart > 1000 * timeout) {
+            return -1;
+        }
+    }
+
+    while (_modemPort->available()) {
+        _modemPort->read();
+    }
+
+    return 0;
+}
+
+
+
+int Modem::sendCmdAndWaitForResp(const char* cmd, const char* resp, unsigned timeout)
+{
+    sendCmd(cmd);
+    return waitForResp(resp, timeout);
+}
+
+
