5e66bc7a1a5baa456166c036d1026985de04e6f9
scottalexgray <scottalexgray@gmail.com>
2020-07-22 13:13:19 +0200

    Added some function declarations in header file

--- Modem.h
+++ Modem.h
@@ -36,8 +36,65 @@ public:
 
 private:
 
+	//Comands used to tranverse the different CIPSTATUS states
 
-	void SetCFUN(int val);
+	void SetCFUN(int val); //AT+CFUN=val //Sets modem functionality
+
+	//state = IP INITIAL
+
+	void SetCSTT(char APN[]); //AT+CSTT=APN //sets APN to use with the simcard, i.e. "internet"
+
+	void SetCPIN(char pin[]); //AT+CPIN=pin //set the pincode for the simcard	
+
+	void GetCPIN(); //AT+CPIN? //gets the pin status of the simcard(does it still need a pin)
+
+	void GetCGREG(CGREG* returnState); //AT+CGREG? //is the sim registered on the network?	
+
+	void SetCGATT(int val); //AT+CGATT=val //Attach to network 
+
+	void SetCGDCONT(int PDPNum, char ipMode[], char APN[]);//AT+CGDCONT=PDPNum,ipMode,APN //Set the PDP context
+
+	/*activate PDP context
+	* Command = AT+CGACT=state,cid
+	* state = 1 : Activate
+	* state = 0 : Deactivate
+	* cid : PDP number (if omitted first one is used)
+	*/
+	void SetCGACT(int state, int cid);
+
+	/*Check if there are any errors
+	* Command = AT+CEER?
+	*check AT command manual for error numbers
+	*/	
+	void GetCEER();
+	
+	//state = IP START
+
+	/*Bring up wireless connection with GPRS or CSD
+	* Command = AT+CIICR
+	*/
+	void SetCIICR();
+
+	//state = IP GPRSACT
+
+	/*Get or Aquire local IP address
+	* Command = AT+CIFSR
+	*/
+	void SetCIFSR();
+
+	//state = IP STATUS, can now register at a bearer and then do HTTP connection
+
+	
+	
+	//Other Commands useful for debugging
+
+	//AT+COPS? //get network info
+
+	//AT+CSQ //gets signal strength
+
+	//AT+CIPSTATUS //Query current connection status and status of modem
+	
+
 
 	void WaitForResponse(int bufferLen, char response[], unsigned long timeoutTime);
 
@@ -49,7 +106,7 @@ private:
 
 	void SendCmdAndWait(const char* cmd);
 
-	void GetCGREG(CGREG* returnState);
+	
 
 
 
